a3c865969f238dbb9700440e39cb30bab72d8c1b
Andrew Orobator <Orobator.Andrew@gmail.com>
2014-02-09 21:50:04 -0500

    Now using the gravity sensor to detect device orientation instead of the orientation vector sensor.

---
+++ app/src/main/java/com/orobator/android/scenerecon/view/activities/SensorTestActivity.java
@@ -0,0 +1,16 @@
+package com.orobator.android.scenerecon.view.activities;
+
+import android.app.Fragment;
+
+import com.orobator.android.scenerecon.view.fragments.SensorTestFragment;
+
+/**
+ * SensorTestActivity is the activity that hosts SensorTestFragment
+ */
+public class SensorTestActivity extends SingleFragmentActivity {
+
+    @Override
+    Fragment createFragment() {
+        return new SensorTestFragment(getApplicationContext());
+    }
+}
